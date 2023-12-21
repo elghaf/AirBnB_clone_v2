3ccb40d39b4de0b71679fab40426cfeaafa2aeaf
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-12-21 03:31:45 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+import os
+from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class"""
+    __tablename__ = 'states'
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete, delete-orphan', backref='state')
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """Returns the cities in this State"""
+            from models import storage
+            return [value for value in storage.all(City).values() if value.state_id == self.id]
+
