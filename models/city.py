3f95a69894f7aed0a948c6fa976f48e2ad308e84
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-12-21 03:29:08 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -3,7 +3,19 @@
 from models.base_model import BaseModel
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """The city class, contains state ID and name"""
+    __tablename__ = 'cities'
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship(
+            'Place',
+            cascade='all, delete, delete-orphan',
+            backref='city'
+        )
+    else:
+        name = ""
+        state_id = ""
+        places = None
