058c434cfc1a7a5666809aa3d670c76d2c7a7663
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-12-24 01:18:00 +0100

    add

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,84 +1,79 @@
 #!/usr/bin/python3
-"""Defines the DBStorage engine."""
+"""New Database Engine"""
+
 from os import getenv
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
-from models.base_model import BaseModel
-from models.amenity import Amenity
+from models.state import State
 from models.city import City
+from models.user import User
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models.user import User
-from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
+from models.amenity import Amenity
 
 
-class DBStorage:
-    """Represents a database storage engine.
-
-    Attributes:
-        __engine (sqlalchemy.Engine): The working SQLAlchemy engine.
-        __session (sqlalchemy.Session): The working SQLAlchemy session.
-    """
-
+class DBStorage():
+    """file storage"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """Initialize a new DBStorage instance."""
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
-                                      format(getenv("HBNB_MYSQL_USER"),
-                                             getenv("HBNB_MYSQL_PWD"),
-                                             getenv("HBNB_MYSQL_HOST"),
-                                             getenv("HBNB_MYSQL_DB")),
+        """Initializing the values and linking the db"""
+        user = getenv("HBNB_MYSQL_USER")
+        password = getenv("HBNB_MYSQL_PWD")
+        host = getenv("HBNB_MYSQL_HOST")
+        db = getenv("HBNB_MYSQL_DB")
+        env = getenv("HBNB_ENV")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, password, host, db),
                                       pool_pre_ping=True)
-        if getenv("HBNB_ENV") == "test":
+        if env == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Query on the curret database session all objects of the given class.
+        """query on the current db session & return a dict"""
 
-        If cls is None, queries all types of objects.
+        my_classes = (Amenity, City, Place, Review, State, User)
+        objects = dict()
 
-        Return:
-            Dict of queried classes in the format <class name>.<obj id> = obj.
-        """
         if cls is None:
-            objs = self.__session.query(State).all()
-            objs.extend(self.__session.query(City).all())
-            objs.extend(self.__session.query(User).all())
-            objs.extend(self.__session.query(Place).all())
-            objs.extend(self.__session.query(Review).all())
-            objs.extend(self.__session.query(Amenity).all())
+            for item in my_classes:
+                query = self.__session.query(item)
+                for obj in query.all():
+                    obj_key = '{}.{}'.format(obj.__class__.name__, obj.id)
+                    objects[obj_key] = obj
         else:
-            if type(cls) == str:
-                cls = eval(cls)
-            objs = self.__session.query(cls)
-        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
+            query = self.__session.query(cls)
+            for obj in query.all():
+                obj_key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+                objects[obj_key] = obj
+        return objects
 
     def new(self, obj):
-        """Add obj to the current database session."""
+        """ add object to the current database session"""
         self.__session.add(obj)
 
     def save(self):
-        """Commit all changes to the current database session."""
+        """commit all the changes"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Delete obj from the current database session."""
-        if obj is not None:
+        """delete current database session"""
+        if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """Create all tables in the database and initialize a new session."""
+        """creates all the tables in database"""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+
+        my_session_maker = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        my_session = scoped_session(my_session_maker)
+        self.__session = my_session()
 
     def close(self):
-        """Close the working SQLAlchemy session."""
+        """close the query"""
         self.__session.close()
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,56 +1,53 @@
 #!/usr/bin/python3
-""" The class to test file storage"""
-import os
+""" Module for testing file storage"""
 import unittest
-
-from models import storage
 from models.base_model import BaseModel
+from models import storage
+import os
 
 
-@unittest.skipIf(
-    os.getenv('HBNB_TYPE_STORAGE') == 'db', 'FileStorage test')
-class TestFileStorage(unittest.TestCase):
-    """ The Class to check the file storage new model """
+class test_fileStorage(unittest.TestCase):
+    """ Class to test the file storage method """
+
     def setUp(self):
-        """ The funct to start the test """
+        """ Set up test environment """
         del_list = []
-        for key in storage.all().keys():
+        for key in storage._FileStorage__objects.keys():
             del_list.append(key)
         for key in del_list:
-            del storage.all()[key]
+            del storage._FileStorage__objects[key]
 
     def tearDown(self):
-        """ the function to Delete the given storage file  """
+        """ Remove storage file at end of tests """
         try:
             os.remove('file.json')
-        except Exception:
+        except:
             pass
 
     def test_obj_list_empty(self):
-        """ The function to check if the objects are empty or not"""
+        """ __objects is initially empty """
         self.assertEqual(len(storage.all()), 0)
 
     def test_new(self):
-        """ The function to chech if the new object succeeded """
+        """ New object is correctly added to __objects """
         new = BaseModel()
-        new.save()
         for obj in storage.all().values():
             temp = obj
         self.assertTrue(temp is obj)
 
     def test_all(self):
-        """ The function to test if the __objects is properly returned """
+        """ __objects is properly returned """
         new = BaseModel()
         temp = storage.all()
         self.assertIsInstance(temp, dict)
 
     def test_base_model_instantiation(self):
-        """ The function to check if the File exists on BaseModel or not """
+        """ File is not created on BaseModel save """
         new = BaseModel()
         self.assertFalse(os.path.exists('file.json'))
 
     def test_empty(self):
-        """ The function to test if the Data is saved to file or not """
+        """ Data is saved to file """
         new = BaseModel()
         thing = new.to_dict()
         new.save()
@@ -58,58 +55,55 @@ class TestFileStorage(unittest.TestCase):
         self.assertNotEqual(os.path.getsize('file.json'), 0)
 
     def test_save(self):
-        """The function to check if the FileStorage succeeded """
+        """ FileStorage save method """
         new = BaseModel()
-        new.save()
+        storage.save()
         self.assertTrue(os.path.exists('file.json'))
 
     def test_reload(self):
-        """ The function to teste if the file is saved to __objects """
+        """ Storage file is successfully loaded to __objects """
         new = BaseModel()
-        new.save()
+        storage.save()
         storage.reload()
-        loaded = None
         for obj in storage.all().values():
             loaded = obj
         self.assertEqual(new.to_dict()['id'], loaded.to_dict()['id'])
 
     def test_reload_empty(self):
-        """ The function to check getting informations from an empty file """
+        """ Load from an empty file """
         with open('file.json', 'w') as f:
             pass
         with self.assertRaises(ValueError):
             storage.reload()
 
     def test_reload_from_nonexistent(self):
-        """ The function to check if sth happens if the file is not there """
+        """ Nothing happens if file does not exist """
         self.assertEqual(storage.reload(), None)
 
     def test_base_model_save(self):
-        """ The function to test the BaseModel storage method """
+        """ BaseModel save method calls storage save """
         new = BaseModel()
         new.save()
         self.assertTrue(os.path.exists('file.json'))
 
     def test_type_path(self):
-        """ The function to check the __file_path type string"""
+        """ Confirm __file_path is string """
         self.assertEqual(type(storage._FileStorage__file_path), str)
 
     def test_type_objects(self):
-        """ The function to check the __objects file type  dict """
+        """ Confirm __objects is a dict """
         self.assertEqual(type(storage.all()), dict)
 
     def test_key_format(self):
-        """The function to check if the  Key is correctely formatted """
+        """ Key is properly formatted """
         new = BaseModel()
         _id = new.to_dict()['id']
-        temp = ''
-        new.save()
-        for key, value in storage.all().items():
-            if value is new:
-                temp = key
+        for key in storage.all().keys():
+            temp = key
         self.assertEqual(temp, 'BaseModel' + '.' + _id)
 
     def test_storage_var_created(self):
-        """ The function to check if the FileStorage object is there """
+        """ FileStorage object storage created """
         from models.engine.file_storage import FileStorage
+        print(type(storage))
         self.assertEqual(type(storage), FileStorage)
