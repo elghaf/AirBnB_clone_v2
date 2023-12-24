e9bcc6042b3fc5d57aedeacfd59319ef42054f7b
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-12-24 02:10:07 +0100

    ;

--- models/base_model.py
+++ models/base_model.py
@@ -24,11 +24,13 @@ class BaseModel:
         '''
             Initialize public instance attributes.
         '''
-        if (len(kwargs) == 0):
+        if not kwargs:
+            # Initialize with default values
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
+            # Update with values from kwargs
             if kwargs.get('created_at'):
                 kwargs["created_at"] = datetime.strptime(
                     kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
@@ -71,17 +73,17 @@ class BaseModel:
         '''
             Return dictionary representation of BaseModel class.
         '''
-        cp_dct = dict(self.__dict__)
-        cp_dct['__class__'] = self.__class__.__name__
-        if 'updated_at' in cp_dct:
-            cp_dct['updated_at'] = self.updated_at.strftime(
+        cp_dict = dict(self.__dict__)
+        cp_dict['__class__'] = self.__class__.__name__
+        if 'updated_at' in cp_dict:
+            cp_dict['updated_at'] = self.updated_at.strftime(
                 "%Y-%m-%dT%H:%M:%S.%f")
-        if 'created_at' in cp_dct:
-            cp_dct['created_at'] = self.created_at.strftime(
+        if 'created_at' in cp_dict:
+            cp_dict['created_at'] = self.created_at.strftime(
                 "%Y-%m-%dT%H:%M:%S.%f")
-        if '_sa_instance_state' in cp_dct:
-            del cp_dct['_sa_instance_state']
-        return (cp_dct)
+        if '_sa_instance_state' in cp_dict:
+            del cp_dict['_sa_instance_state']
+        return cp_dict
 
     def delete(self):
         '''
