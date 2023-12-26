d8be5acc0c402c8b58bd99dafed21b787a5bcb01
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-12-26 15:12:54 +0100

    add

--- console.py
+++ console.py
@@ -1,253 +1,164 @@
 #!/usr/bin/python3
-'''
-    Implementing the console for the HBnB project.
-'''
+""" console """
+
 import cmd
-import json
-import shlex
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+from datetime import datetime
 import models
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+import shlex  # for splitting the line along spaces except in double quotes
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
 
 class HBNBCommand(cmd.Cmd):
-    '''
-        Contains the entry point of the command interpreter.
-    '''
+    """ HBNH console """
+    prompt = '(hbnb) '
 
-    prompt = ("(hbnb) ")
-
-    def do_quit(self, args):
-        '''
-            Quit command to exit the program.
-        '''
+    def do_EOF(self, arg):
+        """Exits console"""
         return True
-
-    def do_EOF(self, args):
-        '''
-            Exits after receiving the EOF signal.
-        '''
-        return True
-
-    def do_create(self, args):
-        '''
-            Create a new instance of class BaseModel and saves it
-            to the JSON file.
-        '''
-        if len(args) == 0:
-            print("** class name missing **")
-            return
-        elif len(args) == 1:
-            try:
-                args = shlex.split(args)
-                class_name = args[0]
-                new_instance = eval(class_name)()
-                new_instance.save()
-                print(new_instance.id)
-
-            except:
-                print("** class doesn't exist **")
-        else:
-            try:
-                args = shlex.split(args)
-                class_name = args.pop(0)
-                obj = eval(class_name)()
-                for arg in args:
-                    attribute, value = arg.split('=')
-                    if hasattr(obj, attribute):
-                        try:
-                            value = eval(value)
-                        except:
-                            value = value.replace('_',' ')
-                        setattr(obj, attribute, value)
-
-                obj.save()
-            except:
-                return
-            print(obj.id)
-
-    def do_show(self, args):
-        '''
-            Print the string representation of an instance based on
-            the class name and id given as args.
-        '''
-        args = shlex.split(args)
-        if len(args) == 0:
-            print("** class name missing **")
-            return
-        if len(args) == 1:
-            print("** instance id missing **")
-            return
-
-        models.storage.reload()
-        objects_dict = models.storage.all()
-        try:
-            class_to_show = eval(args[0])
-        except NameError:
-            print("** class doesn't exist **")
-            return
-
-        instance_key = args[0] + "." + args[1]
-        try:
-            instance_value = objects_dict[instance_key]
-            print(instance_value)
-        except KeyError:
-            print("** no instance found **")
-
-    def do_destroy(self, args):
-        '''
-            Deletes an instance based on the class name and id.
-        '''
-        args = shlex.split(args)
-        if len(args) == 0:
-            print("** class name missing **")
-            return
-        elif len(args) == 1:
-            print("** instance id missing **")
-            return
-
-        class_name = args[0]
-        class_id = args[1]
-
-        models.storage.reload()
-        objects_dict = models.storage.all()
-        try:
-            class_to_destroy = eval(class_name)
-        except NameError:
-            print("** class doesn't exist **")
-            return
-
-        instance_key = class_name + "." + class_id
-        try:
-            del objects_dict[instance_key]
-        except KeyError:
-            print("** no instance found **")
-        models.storage.save()
-
-    def do_all(self, args):
-        '''
-            Prints all string representation of all instances
-            based or not on the class name.
-        '''
-        instances_list = []
-
-        models.storage.reload()
-        all_objects = models.storage.all()
-        try:
-            if len(args) != 0:
-                class_to_filter = eval(args)
-        except NameError:
-            print("** class doesn't exist **")
-            return
-
-        for key, instance in all_objects.items():
-            if len(args) != 0:
-                if isinstance(instance, class_to_filter):
-                    instances_list.append(instance)
-            else:
-                instances_list.append(instance)
-
-        print(instances_list)
-
-    def do_update(self, args):
-        '''
-            Update an instance based on the class name and id
-            sent as args.
-        '''
-        models.storage.reload()
-        args = shlex.split(args)
-        if len(args) == 0:
-            print("** class name missing **")
-            return
-        elif len(args) == 1:
-            print("** instance id missing **")
-            return
-        elif len(args) == 2:
-            print("** attribute name missing **")
-            return
-        elif len(args) == 3:
-            print("** value missing **")
-            return
-
-        try:
-            class_to_update = eval(args[0])
-        except NameError:
-            print("** class doesn't exist **")
-            return
-
-        instance_key = args[0] + "." + args[1]
-        objects_dict = models.storage.all()
-        try:
-            instance_value = objects_dict[instance_key]
-        except KeyError:
-            print("** no instance found **")
-            return
-
-        try:
-            attribute_type = type(getattr(instance_value, args[2]))
-            args[3] = attribute_type(args[3])
-        except AttributeError:
-            pass
-
-        setattr(instance_value, args[2], args[3])
-        instance_value.save()
 
     def emptyline(self):
-        '''
-            Prevents printing anything when an empty line is passed.
-        '''
-        pass
+        """ overwriting the emptyline method """
+        return False
 
-    def do_count(self, args):
-        '''
-            Counts/retrieves the number of instances.
-        '''
-        instances_list = []
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
 
-        models.storage.reload()
-        all_objects = models.storage.all()
-        try:
-            if len(args) != 0:
-                class_to_count = eval(args)
-        except NameError:
+    def _key_value_parser(self, args):
+        """creates a dictionary from a list of strings"""
+        new_dict = {}
+        for arg in args:
+            if "=" in arg:
+                kvp = arg.split('=', 1)
+                key = kvp[0]
+                value = kvp[1]
+                if value[0] == value[-1] == '"':
+                    value = shlex.split(value)[0].replace('_', ' ')
+                else:
+                    try:
+                        value = int(value)
+                    except:
+                        try:
+                            value = float(value)
+                        except:
+                            continue
+                new_dict[key] = value
+        return new_dict
+
+    def do_create(self, arg):
+        """Creates a new instance of a class"""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] in classes:
+            new_dict = self._key_value_parser(args[1:])
+            instance = classes[args[0]](**new_dict)
+        else:
             print("** class doesn't exist **")
-            return
+            return False
+        print(instance.id)
+        instance.save()
 
-        for key, instance in all_objects.items():
-            if len(args) != 0:
-                if isinstance(instance, class_to_count):
-                    instances_list.append(instance)
+    def do_show(self, arg):
+        """Prints an instance as a string based on the class and id"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] in classes:
+            if len(args) > 1:
+                key = args[0] + "." + args[1]
+                if key in models.storage.all():
+                    print(models.storage.all()[key])
+                else:
+                    print("** no instance found **")
             else:
-                instances_list.append(instance)
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 
-        print(len(instances_list))
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class and id"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] in classes:
+            if len(args) > 1:
+                key = args[0] + "." + args[1]
+                if key in models.storage.all():
+                    models.storage.all().pop(key)
+                    models.storage.save()
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 
-    def default(self, args):
-        '''
-            Catches all the function names that are not explicitly defined.
-        '''
-        functions = {"all": self.do_all, "update": self.do_update,
-                     "show": self.do_show, "count": self.do_count,
-                     "destroy": self.do_destroy, "update": self.do_update}
-        args = (args.replace("(", ".").replace(")", ".")
-                .replace('"', "").replace(",", "").split("."))
+    def do_all(self, arg):
+        """Prints string representations of instances"""
+        args = shlex.split(arg)
+        obj_list = []
+        if len(args) == 0:
+            obj_dict = models.storage.all()
+        elif args[0] in classes:
+            obj_dict = models.storage.all(classes[args[0]])
+        else:
+            print("** class doesn't exist **")
+            return False
+        for key in obj_dict:
+            obj_list.append(str(obj_dict[key]))
+        print("[", end="")
+        print(", ".join(obj_list), end="")
+        print("]")
 
-        try:
-            command_argument = args[0] + " " + args[2]
-            function_to_call = functions[args[1]]
-            function_to_call(command_argument)
-        except:
-            print("*** Unknown syntax:", args[0])
+    def do_update(self, arg):
+        """Update an instance based on the class name, id, attribute & value"""
+        args = shlex.split(arg)
+        integers = ["number_rooms", "number_bathrooms", "max_guest",
+                    "price_by_night"]
+        floats = ["latitude", "longitude"]
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] in classes:
+            if len(args) > 1:
+                k = args[0] + "." + args[1]
+                if k in models.storage.all():
+                    if len(args) > 2:
+                        if len(args) > 3:
+                            if args[0] == "Place":
+                                if args[2] in integers:
+                                    try:
+                                        args[3] = int(args[3])
+                                    except:
+                                        args[3] = 0
+                                elif args[2] in floats:
+                                    try:
+                                        args[3] = float(args[3])
+                                    except:
+                                        args[3] = 0.0
+                            setattr(models.storage.all()[k], args[2], args[3])
+                            models.storage.all()[k].save()
+                        else:
+                            print("** value missing **")
+                    else:
+                        print("** attribute name missing **")
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 
-if __name__ == "__main__":
-    '''
-        Entry point for the loop.
-    '''
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,121 +1,88 @@
 #!/usr/bin/python3
-'''
-    Tests for db_storage module
-'''
-import os
-import json
-import unittest
-import pep8
-import models
-from models.base_model import BaseModel, Base
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.user import User
+"""
+Contains the TestDBStorageDocs and TestDBStorage classes
+"""
+
 from datetime import datetime
+import inspect
+import models
 from models.engine import db_storage
 from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
 from models.review import Review
-from models.engine.file_storage import FileStorage
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.orm import sessionmaker
+from models.state import State
+from models.user import User
+import json
+import os
+import pep8
+import unittest
+DBStorage = db_storage.DBStorage
+classes = {"Amenity": Amenity, "City": City, "Place": Place,
+           "Review": Review, "State": State, "User": User}
 
 
-@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'not db storage')
-class TestDBStorage(unittest.TestCase):
-    '''
-    Tests to check the documentation and style of DBStorage class
-    '''
+class TestDBStorageDocs(unittest.TestCase):
+    """Tests to check the documentation and style of DBStorage class"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.dbs_f = inspect.getmembers(DBStorage, inspect.isfunction)
 
     def test_pep8_conformance_db_storage(self):
-        '''
-        Test that db_storage.py conforms to PEP8.
-        '''
+        """Test that models/engine/db_storage.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['models/engine/db_storage.py'])
-        self.assertEqual(result.total_errors, 0, "Pep8 Errors")
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test_db_storage(self):
+        """Test tests/test_models/test_db_storage.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_engine/\
+test_db_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
     def test_db_storage_module_docstring(self):
-        '''
-        Test for the db_storage.py module docstring
-        '''
-        self.assertIsNot(db_storage.__doc__, None, "Missing docstring")
+        """Test for the db_storage.py module docstring"""
+        self.assertIsNot(db_storage.__doc__, None,
+                         "db_storage.py needs a docstring")
+        self.assertTrue(len(db_storage.__doc__) >= 1,
+                        "db_storage.py needs a docstring")
 
     def test_db_storage_class_docstring(self):
-        '''
-        Test for the DBStorage class docstring
-        '''
-        self.assertIsNot(DBStorage.__doc__, None, "Missing docstring")
+        """Test for the DBStorage class docstring"""
+        self.assertIsNot(DBStorage.__doc__, None,
+                         "DBStorage class needs a docstring")
+        self.assertTrue(len(DBStorage.__doc__) >= 1,
+                        "DBStorage class needs a docstring")
 
     def test_dbs_func_docstrings(self):
-        '''
-        Test for docstrings in DBStorage methods
-        '''
+        """Test for the presence of docstrings in DBStorage methods"""
         for func in self.dbs_f:
             self.assertIsNot(func[1].__doc__, None,
                              "{:s} method needs a docstring".format(func[0]))
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
 
-    def test_State(self):
-        '''
-            Tests State class
-        '''
-        state = State(name='California')
-        if state.id in models.storage.all():
-            self.assertTrue(state.name, "California")
 
-    def test_City(self):
-        '''
-            Tests City class
-        '''
-        city = City(name='San Francisco')
-        if city.id in models.storage.all():
-            self.assertTrue(city.name, 'San Francisco')
+class TestFileStorage(unittest.TestCase):
+    """Test the FileStorage class"""
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_all_returns_dict(self):
+        """Test that all returns a dictionaty"""
+        self.assertIs(type(models.storage.all()), dict)
 
-    def test_User(self):
-        '''
-            Tests User class
-        '''
-        new_user = User(
-            email='boop@boop.com',
-            password='boopword',
-            first_name='boop',
-            last_name='poob')
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_all_no_class(self):
+        """Test that all returns all rows when no class is passed"""
 
-        self.assertIsInstance(new_user.email, str)
-        self.assertIsInstance(new_user.password, str)
-        self.assertIsInstance(new_user.first_name, str)
-        self.assertIsInstance(new_user.last_name, str)
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_new(self):
+        """test that new adds an object to the database"""
 
-        self.assertEqual(new_user.password, 'boopword')
-        self.assertEqual(new_user.first_name, 'boop')
-        self.assertEqual(new_user.last_name, 'poob')
-
-    def testPlace(self):
-        '''
-        Tests Place class
-        '''
-        place = Place(name="Home")
-        if place.id in models.storage.all():
-            self.assertTrue(place.name, "Home")
-
-    def testAmenity(self):
-        '''
-        Tests Amenity class
-        '''
-        amenity = Amenity(name='sink')
-        if amenity.id in models.storage.all():
-            self.assertTrue(amenity.name, 'sink')
-
-    def testReview(self):
-        '''
-        Test Review class
-        '''
-        review = Review(text='Nice place')
-        if review.id in models.storage.all():
-            self.assertTrue(review.text, 'Nice place')
-
-    def teardown(self):
-        self.session.close()
-        self.session.rollback()
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_save(self):
+        """Test that save properly saves objects to file.json"""
